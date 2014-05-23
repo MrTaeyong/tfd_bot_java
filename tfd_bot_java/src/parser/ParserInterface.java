0c4b450fa700fe97cde87e7cff4f062a8048206b
NK <beohemian@gmail.com>
2014-05-23 17:11:29 +0900

    interface 추가.

---
+++ tfd_bot_java/src/parser/ParserInterface.java
@@ -0,0 +1,25 @@
+/**
+ * @FileName 	: ParserInterface.java
+ * @Project 	: tfd_bot_java
+ * @Date 		: 2014. 5. 23.
+ * @Author 		: NCri
+ */
+package parser;
+
+/**
+ * @Class		: ParserInterface
+ * @Date 		: 2014. 5. 23.
+ * @Author 		: NCri
+ */
+public interface ParserInterface {
+	/**
+	 * 
+	 * @method Name	: getResult
+	 * @date   		: 2014. 5. 23. 
+	 * @author   	: NCri
+	 * @description :
+	 */
+	public void getResult();
+	
+	
+}
