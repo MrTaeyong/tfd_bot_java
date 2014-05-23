2b7b33d4403f5e8189eb4c2a2388450834107f03
NK <beohemian@gmail.com>
2014-05-23 17:04:26 +0900

    Google Place Class 추가.

---
+++ tfd_bot_java/src/parser/google/GooglePlace.java
@@ -0,0 +1,16 @@
+/**
+ * @FileName 	: GooglePlace.java
+ * @Project 	: tfd_bot_java
+ * @Date 		: 2014. 5. 23.
+ * @Author 		: NCri
+ */
+package parser.google;
+
+/**
+ * @Class		: GooglePlace
+ * @Date 		: 2014. 5. 23.
+ * @Author 		: NCri
+ */
+public class GooglePlace {
+
+}
