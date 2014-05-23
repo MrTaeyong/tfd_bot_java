53a2ed7bd8db9508f95eee23b94127ba46c751ec
NK <beohemian@gmail.com>
2014-05-23 18:52:16 +0900

    modify connector interface

--- tfd_bot_java/src/parser/ConnectorInterface.java
+++ tfd_bot_java/src/parser/ConnectorInterface.java
@@ -1,7 +1,6 @@
 package parser;
 
-import java.util.Map;
-
-public interface ConnectorInterface {
-	public Object connect(String url, Map<String, String> param);
+public interface ConnectorInterface {	
+	public ConnectorInterface getInstance(int type);
+	public Object connect();
 }
