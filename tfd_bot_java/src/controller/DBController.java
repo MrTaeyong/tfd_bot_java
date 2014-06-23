20366678876e02db9892c82ac254d91b63214a86
skarl86 <beohemian@gmail.com>
2014-06-23 23:21:12 +0900

    Datebase Enum Type 추가.

--- tfd_bot_java/src/controller/DBController.java
+++ tfd_bot_java/src/controller/DBController.java
@@ -18,6 +18,32 @@ import java.util.Map;
  * @Author : NCri
  */
 public class DBController extends Controller {
+	/**
+	 * +----------+--------------+------+-----+---------+-------+
+	 * | Field    | Type         | Null | Key | Default | Extra |
+	 * +----------+--------------+------+-----+---------+-------+
+	 * | local_id | int(10)      | NO   | PRI | NULL    |       |
+	 * | link     | varchar(100) | NO   | PRI | NULL    |       |
+	 * | width    | int(4)       | YES  |     | NULL    |       |
+	 * | height   | int(4)       | YES  |     | NULL    |       |
+	 * +----------+--------------+------+-----+---------+-------+
+	 */
+	public static enum PlaceLink{
+		LOCAL_ID("local_id"), LINK("link"), WIDTH("width"), HEIGHT("height") ;
+		final private String name;
+		
+		PlaceLink(String name) {
+		        this.name = name;
+		    }
+		    String value() {
+		        return name;
+		}
+	}
+	
+	public static enum Place{
+		
+	}
+	
 	private final String _BASE_URL = "jdbc:mysql://203.253.23.40/tfd";
 	
 	public static final String PLACE_FIELD_NAME = "name";
@@ -29,8 +55,45 @@ public class DBController extends Controller {
 	public static final String PLACE_FIELD_POINT_X = "pointx";
 	public static final String PLACE_FIELD_POINT_Y = "pointy";
 	
-	public void getData(String query){
+	public void getConnection(String baseUrl, String id, String passwd){
 		
+	}
+	public Map<String, String> getData(String query){
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection(_BASE_URL, "dbmaster", "tfd1234");
+			System.out.println("DBms connection success");
+			System.out.println("DB load success");
+			
+			// create query string.
+			String queryFormat = " insert into " + tableName + " (name, category, address, telephone, url, description, pointx, pointy)"
+					+ " values (?, ?, ?, ?, ?, ?)";
+
+			// create the mysql insert preparedstatement
+			PreparedStatement preparedStmt = con.prepareStatement(queryFormat);
+
+			for(Map<String, String> data : insertDatas){
+				preparedStmt.setString(1, data.get(DBController.PLACE_FIELD_NAME));
+				preparedStmt.setString(2, data.get(DBController.PLACE_FIELD_CATE));
+				preparedStmt.setString(4, data.get(DBController.PLACE_FIELD_TELE));
+				preparedStmt.setString(3, data.get(DBController.PLACE_FIELD_ADDR));
+				preparedStmt.setString(5, data.get(DBController.PLACE_FIELD_URL));
+				preparedStmt.setString(6, data.get(DBController.PLACE_FIELD_DESC));
+
+				// execute the preparedstatement
+				preparedStmt.execute();
+			}
+
+			con.close();
+			System.out.println("데이터 Insert 완료.");
+			System.out.println("DB 접속 종료.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 	/**
 	 * 
